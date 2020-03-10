e4a648b0863353afed13c5e2013c8f56595b9952
Nasir Khan Saikat <nasir8891@gmail.com>
2020-03-10 12:55:03 +0600

    File Upload

---
+++ upload.php
@@ -0,0 +1,93 @@
+<?php
+// print_r($_POST);
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["file_field"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["file_field"]["tmp_name"]);
+        if($check !== false) {
+            $error_message = "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            $error_message = "File is not an image.";
+            $uploadOk = 0;
+        }
+    }
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        $error_message = "Sorry, file already exists.";
+        $uploadOk = 0;
+    }
+    // Check file size
+    if ($_FILES["file_field"]["size"] > 500000) {
+        $error_message = "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+        $error_message = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        $error_message = "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["file_field"]["tmp_name"], $target_file)) {
+            $success_message = "The file ". basename( $_FILES["file_field"]["name"]). " has been uploaded.";
+        } else {
+            $error_message = "Sorry, there was an error uploading your file.";
+        }
+    }
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Calculator</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-12 col-sm-6 align-self-center">
+                <div class="card">
+                    <div class="card-body">
+                        <h1 class="text-center">File Upload</h1>
+                        <?php if($error_message!=''): ?>
+                            <div class="alert alert-warning text-center">
+                                <?php echo $error_message; ?>
+                            </div>
+                        <?php endif; ?>
+                        <?php if($success_message!=''): ?>
+                            <div class="alert alert-success text-center">
+                                <?php echo $success_message; ?>
+                            </div>
+                        <?php endif; ?>
+                        <form action="upload.php" method="post" enctype="multipart/form-data">
+                            <div class="form-group">
+                                <label for="total">Select image to upload:</label>
+                                <input type="file" name="file_field" id="file_field">
+                            </div>
+
+
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

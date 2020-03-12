0a27c776343b6252d37df4a6f2fc1562dd808c82
Nasir Khan Saikat <nasir8891@gmail.com>
2020-03-12 16:35:54 +0600

    Include

---
+++ _footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-12 col-sm-6 align-self-center">
+                <p class="text-center">
+                    Get the source code at <a href="https://github.com/nasirkhan/php-web/" target="_blank">https://github.com/nasirkhan/php-web</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
--- upload.php
+++ upload.php
@@ -51,10 +51,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <?php include "_header.php" ?>
 
     <title>Calculator</title>
 </head>
@@ -89,5 +86,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
             </div>
         </div>
     </div>
+    <?php include "_footer.php" ?>
 </body>
 </html>

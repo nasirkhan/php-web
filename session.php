1f23b42d07abbfd72546f34d4a2d02f2fa054a32
Nasir Khan Saikat <nasir8891@gmail.com>
2020-03-19 10:21:24 +0600

    session

---
+++ session.php
@@ -0,0 +1,32 @@
+<?php
+// Start the session
+session_start();
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <?php require_once "_header.php" ?>
+
+        <title>Session</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row justify-content-center mt-5">
+                <div class="col-12 col-sm-6 align-self-center">
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <h1>Session</h1>
+                            <hr>
+                            <?php
+                            // Storing session data
+                            echo "<br> Name: ".$_SESSION["name"] = "Nasir Khan";
+                            echo "<br> Website: ".$_SESSION["website"] = "nasirkhn.com";
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php include_once "_footer.php" ?>
+    </body>
+</html>

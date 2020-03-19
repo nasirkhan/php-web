8dba1fc9de7b52e588a96cc1de10bff257d3b061
Nasir Khan Saikat <nasir8891@gmail.com>
2020-03-19 11:07:00 +0600

    PHP Elephat Added

--- _navbar.php
+++ _navbar.php
@@ -1,6 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="index.php">PHP</a>
+        <a class="navbar-brand" href="index.php">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/3/31/Webysther_20160423_-_Elephpant.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+            PHP
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>

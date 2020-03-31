38e2c0a8968a01655b02d04f2f0eb17b2b64367f
Nasir Khan Saikat <nasir8891@gmail.com>
2020-04-01 01:01:26 +0600

    + regular expressions

--- regex.php
+++ regex.php
@@ -36,6 +36,19 @@ session_start();
                             }
                             ?>
                             <hr>
+                            <?php
+                            $pattern = "/ca[kf]e/";
+                            $text = "He was eating cake in the cafe.";
+                            echo "Pattern: $pattern <br>";
+                            echo "Text: $text <br>";
+                            if(preg_match($pattern, $text)){
+                                echo "Match found!";
+                            } else{
+                                echo "Match not found.";
+                            }
+                            ?>
+
+                            <hr>
                             <h3>
                                 preg_split()
                             </h3>
@@ -49,6 +62,19 @@ session_start();
 
                             print_r($return_array );
                             ?>
+                            <hr>
+                            <?php
+                            $pattern = "/[\s,]+/";
+                            $text = "My favourite colors are red, green and blue";
+                            echo "String: $text <br>";
+                            $parts = preg_split($pattern, $text);
+
+                            // Loop through parts array and display substrings
+                            foreach($parts as $part){
+                                echo $part . "<br>";
+                            }
+                            ?>
+
                             <hr>
                             <h3>
                                 preg_replace()
@@ -65,6 +91,74 @@ session_start();
 
                             echo "$string_new";
                             ?>
+
+                            <hr>
+
+                            <?php
+                            $pattern = "/\s/";
+                            $replacement = "-";
+                            $text = "Earth revolves around\nthe\tSun";
+
+                            echo "Pattern: $pattern <br>";
+                            echo "Text: <pre>$text</pre> <br>";
+
+                            // Replace spaces, newlines and tabs
+                            $text_new = preg_replace($pattern, $replacement, $text);
+                            echo "Text: <pre>$text_new</pre> <br>";
+                            // Replace only spaces
+                            $text_new = str_replace(" ", "-", $text);
+                            echo "Text: <pre>$text_new</pre> <br>";
+                            ?>
+
+                            <hr>
+                            <?php
+                            $pattern = '/\bcar\w*/';
+                            $replacement = '<b>$0</b>';
+                            $text = 'Words begining with car: cart, carrot, cartoon. Words ending with car: scar, oscar, supercar.';
+
+                            echo "Pattern: $pattern <br>";
+                            echo "Text: <pre>$text</pre> <br>";
+
+                            $text_new = preg_replace($pattern, $replacement, $text);
+                            echo "New Text: <pre>$text_new</pre> <br>";
+                            ?>
+
+
+
+                            <hr>
+                            <h3>
+                                preg_grep()
+                            </h3>
+                            <hr>
+                            <?php
+                            $pattern = "/^B/";
+                            $names = array("Bangladesh", "Nepal", "Bhutan");
+                            echo "Names: ";
+                            print_r($names);
+                            $matches = preg_grep($pattern, $names);
+
+                            // Loop through matches array and display matched names
+                            echo "<br><br>Names starts with B<br>";
+                            foreach($matches as $match){
+                                echo $match . "<br>";
+                            }
+                            ?>
+
+                            <hr>
+                            <h3>
+                                preg_match_all()
+                            </h3>
+                            <hr>
+                            <?php
+                            $pattern = "/color/i";
+                            $text = "Color red is more visible than color blue in daylight.";
+                            echo "Pattern: $pattern <br>";
+                            echo "Text: $text <br>";
+
+                            $matches = preg_match_all($pattern, $text, $array);
+                            echo $matches . " matches were found.";
+                            ?>
+
                         </div>
                     </div>
                 </div>
